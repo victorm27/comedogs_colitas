0e6085a14fdd665c67e8ce043eec199cf054b349
victorm27 <victormanuel272791@gmail.com>
2023-11-18 18:22:40 -0500

    frontend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Login from './components/Login';
 import './styles/styles.min.css';
 import CrudUsers from './components/CrudUsers'
 import Register from './components/Register';
-import ForgotPassword from './components/ForgotPassword'; 
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
            <Route exact path="/" element={<Login />} />
            <Route path="/register" element={<Register />} />
            <Route path="/users" element={<CrudUsers />} />
-           <Route path='/forgotpassword' element={<ForgotPassword />} /> {/* Utiliza el nombre en mayúsculas aquí */}
          </Routes>
        </div>
      </Router>
